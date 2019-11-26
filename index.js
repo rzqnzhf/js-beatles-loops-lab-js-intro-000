27933744843d0780dabad7aba8ae2e398f8a999b
Skin <rizqinnazhif@gmail.com>
2019-11-26 19:39:03 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 function theBeatlesPlay(arr1, arr2) {
   var b = [];
   for (var i = 0; i < 5; i++) {
-    `${arr1[i]} plays ${arr2[i]}`
+    var x = `${arr1[i]} plays ${arr2[i]}`;
+    b.push(x);
   }
+  return b;
 }
